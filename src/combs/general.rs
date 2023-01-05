6a18b807cd1c4032a9f1afbfb4245e49570fd9b0
Steven Xiang <wgxh-cli@qq.com>
2023-01-05 14:11:45 +0800

    [combs] Finished `Inspect` combinator

--- src/combs/general.rs
+++ src/combs/general.rs
@@ -1,1 +1,11 @@
+//! The `general` module defined some context-free combinators.
+//!
+//! In rust, that is, will not specialize the generics of `Parse`.
+//!
+//! There are the defined combinator module below:
+//! * map - A -> B -> C => A -> C
+
 pub mod maps;
+pub mod inspect;
+
+pub use maps::*;
