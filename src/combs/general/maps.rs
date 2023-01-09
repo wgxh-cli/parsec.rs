50c858530731814e97cccc730e8a49225ab1203c
Steven Xiang <wgxh-cli@qq.com>
2023-01-09 14:26:50 +0800

    [combs] Simplified the implementation of `MapFn`

--- src/combs/general/maps.rs
+++ src/combs/general/maps.rs
@@ -21,29 +21,15 @@ impl<'a, A, B, C> Map<'a, A, B, C> {
     }
   }
 }
+pub fn map<'a, A, B, C>(mapper: impl Parse<'a, B, C> + 'a) -> Map<'a, A, B, C> {
+  Map::new(mapper)
+}
 
 /// `MapFn` is actually a shortcut for `Map::new(with_fn(...))`
 ///
 /// See also `Map`
-pub struct MapFn<'a, A, B, C> {
-  mapper: Box<dyn Fn(B) -> C + 'a>,
-  _marker: PhantomData<&'a A>,
-}
-impl<'a, A, B, C> Comb<'a, A, B, C> for MapFn<'a, A, B, C> {
-  fn comb(
-    self,
-    parser: &'a (impl Parse<'a, A, B> + 'a)
-  ) -> BoxedParser<'a, A, C> where Self: 'a {
-    Map::new(with_fn(move |b| (self.mapper)(b))).comb(parser)
-  }
-}
-impl<'a, A, B, C> MapFn<'a, A, B, C> {
-  pub fn new(mapper: impl Fn(B) -> C + 'a) -> Self {
-    MapFn {
-      mapper: Box::new(mapper),
-      _marker: PhantomData,
-    }
-  }
+pub fn map_fn<'a, A: 'a, B: 'a, C: 'a>(mapper: impl Fn(B) -> C + 'a) -> Map<'a, A, B, C> {
+  Map::new(with_fn(mapper))
 }
 
 // NOTICE! This is just a test to find possible lifetime problems in use.
@@ -51,7 +37,7 @@ impl<'a, A, B, C> MapFn<'a, A, B, C> {
 #[cfg(test)]
 mod test {
   use crate::prelude::*;
-  use super::MapFn;
+  use super::map_fn;
 
   pub struct Next;
   impl<'a> Parse<'a, String, char> for Next {
@@ -67,7 +53,7 @@ mod test {
     assert_eq!(Next.parse(test_suit.clone()), 'S');
     assert_eq!(
       Next
-        .pipe(MapFn::new(|c: char| {
+        .pipe(map_fn(|c: char| {
           c.to_lowercase().to_string()
         }))
         .parse(test_suit),
