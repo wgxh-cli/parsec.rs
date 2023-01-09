1748c86c8b6b2a9af57c19c9b8901629ef58b9b5
Steven Xiang <wgxh-cli@qq.com>
2023-01-09 14:39:05 +0800

    [combs] Simplified `errproc` module added `MapOk` and `MapErr` combinators

--- src/combs/errproc.rs
+++ src/combs/errproc.rs
@@ -1,59 +1,9 @@
 //! # `errproc`
 //!
 //! The series of `errproc` combinators are used to adapt Rust's builtin types.
-//! Such as, `Result` and `Option`, and there's some traits to generalize these combinators.
+//! Such as, `Result` or `Option`, and there's some traits to generalize these combinators.
 //!
 //! They are usually useful when work with return types with both success and failure states.
 //! But these traits enable you to use your own types instead of builtin types.
 
 pub mod maps;
-
-pub enum ResStates<T, E> {
-  Succeed(T),
-  Failed(E),
-}
-
-pub trait Res<T, E> {
-  fn to_state(&self) -> ResStates<T, E>;
-  fn from_state(state: ResStates<T, E>) -> Self;
-  #[inline]
-  fn succeed(&self) -> bool {
-    if let ResStates::<T, E>::Succeed(_) = self.to_state() {
-      true
-    } else {
-      false
-    }
-  }
-  #[inline]
-  fn failed(&self) -> bool {
-    !self.succeed()
-  }
-}
-impl<T: Clone, E: Clone> Res<T, E> for Result<T, E> {
-  fn to_state(&self) -> ResStates<T, E> {
-    match self {
-      Ok(result) => ResStates::Succeed(result.clone()),
-      Err(err) => ResStates::Failed(err.clone()),
-    }
-  }
-  fn from_state(state: ResStates<T, E>) -> Self {
-    match state {
-      ResStates::Succeed(result) => Ok(result),
-      ResStates::Failed(err) => Err(err),
-    }
-  }
-}
-impl<T: Clone> Res<T, ()> for Option<T> {
-  fn to_state(&self) -> ResStates<T, ()> {
-    match self {
-      Some(result) => ResStates::Succeed(result.clone()),
-      None => ResStates::Failed(()),
-    }
-  }
-  fn from_state(state: ResStates<T, ()>) -> Self {
-    match state {
-      ResStates::Succeed(result) => Some(result),
-      ResStates::Failed(_) => None,
-    }
-  }
-}
--- src/combs/errproc/maps.rs
+++ src/combs/errproc/maps.rs
@@ -1,68 +1,62 @@
 use crate::prelude::*;
 use std::marker::PhantomData;
 
-pub struct MapOk<'a, I, T, E, O> {
-  mapper: Box<dyn Fn(T) -> O + 'a>,
+pub struct MapOk<'a, I, O1, O2, E> {
+  mapper: Box<dyn Fn(O1) -> O2 + 'a>,
   _marker: PhantomData<(I, E)>,
 }
-impl<'a, I, T, E, O, S1, S2> Comb<'a, I, S1, S2> for MapOk<'a, I, T, E, O>
-where
-  S1: Res<T, E> + 'a,
-  S2: Res<O, E> + 'a,
-{
+impl<'a, I, O1, O2, E> Comb<'a, I, Result<O1, E>, Result<O2, E>> for MapOk<'a, I, O1, O2, E> {
   fn comb(
     self,
-    parser: &'a (impl Parse<'a, I, S1> + 'a)
-  ) -> BoxedParser<'a, I, S2> where Self: 'a {
-    with_fn(move |i| {
-      match parser.parse(i).to_state() {
-        ResStates::Succeed(result) => S2::from_state(ResStates::Succeed((self.mapper)(result))),
-        ResStates::Failed(err) => S2::from_state(ResStates::Failed(err))
-      }
-    }).boxed()
+    parser: &'a (impl Parse<'a, I, Result<O1, E>> + 'a)
+  ) -> BoxedParser<'a, I, Result<O2, E>> where Self: 'a {
+    parser
+      .pipe(map_fn(move |res: Result<O1, E>| {
+        res.map(|res| {
+          (self.mapper)(res)
+        })
+      }))
+      .boxed()
   }
 }
-impl<'a, I, T, E, O> MapOk<'a, I, T, E, O> {
-  pub fn new(mapper: impl Fn(T) -> O + 'a) -> Self {
+impl<'a, I, O1, O2, E> MapOk<'a, I, O1, O2, E> {
+  pub fn new(mapper: impl Fn(O1) -> O2 + 'a) -> Self {
     MapOk {
       mapper: Box::new(mapper),
       _marker: PhantomData,
     }
   }
 }
-pub fn map_ok<'a, I, T, E, O>(mapper: impl Fn(T) -> O + 'a) -> MapOk<'a, I, T, E, O> {
+pub fn map_ok<'a, I, O1, O2, E>(mapper: impl Fn(O1) -> O2 + 'a) -> MapOk<'a, I, O1, O2, E> {
   MapOk::new(mapper)
 }
 
-pub struct MapErr<'a, I, T, E, O> {
-  mapper: Box<dyn Fn(E) -> O + 'a>,
-  _marker: PhantomData<(I, T)>
+pub struct MapErr<'a, I, O, E1, E2> {
+  mapper: Box<dyn Fn(E1) -> E2 + 'a>,
+  _marker: PhantomData<(I, O)>,
 }
-impl<'a, I, T, E, O, S1, S2> Comb<'a, I, S1, S2> for MapErr<'a, I, T, E, O>
-where
-  S1: Res<T, E> + 'a,
-  S2: Res<T, O> + 'a,
-{
+impl<'a, I, O, E1, E2> Comb<'a, I, Result<O, E1>, Result<O, E2>> for MapErr<'a, I, O, E1, E2> {
   fn comb(
     self,
-    parser: &'a (impl Parse<'a, I, S1> + 'a)
-  ) -> BoxedParser<'a, I, S2> where Self: 'a {
-    with_fn(move |i| {
-      match parser.parse(i).to_state() {
-        ResStates::Failed(err) => S2::from_state(ResStates::Failed((self.mapper)(err))),
-        ResStates::Succeed(result) => S2::from_state(ResStates::Succeed(result)),
-      }
-    }).boxed()
+    parser: &'a (impl Parse<'a, I, Result<O, E1>> + 'a)
+  ) -> BoxedParser<'a, I, Result<O, E2>> where Self: 'a {
+    parser
+      .pipe(map_fn(move |res: Result<O, E1>| {
+        res.map_err(|err| {
+          (self.mapper)(err)
+        })
+      }))
+      .boxed()
   }
 }
-impl<'a, I, T, E, O> MapErr<'a, I, T, E, O> {
-  pub fn new(mapper: impl Fn(E) -> O + 'a) -> Self {
+impl<'a, I, O, E1, E2> MapErr<'a, I, O, E1, E2> {
+  pub fn new(mapper: impl Fn(E1) -> E2 + 'a) -> Self {
     MapErr {
       mapper: Box::new(mapper),
       _marker: PhantomData,
     }
   }
 }
-pub fn map_err<'a, I, T, E, O>(mapper: impl Fn(E) -> O + 'a) -> MapErr<'a, I, T, E, O> {
+pub fn map_err<'a, I, O, E1, E2>(mapper: impl Fn(E1) -> E2 + 'a) -> MapErr<'a, I, O, E1, E2> {
   MapErr::new(mapper)
 }
